01e4f770ef3042f376c4878a5cebe13a66ceb5b6
sjubusel <sju.busel@gmail.com>
2019-10-12 12:16:31 +0200

    CS - Tasks A, Tasks B1, B2 of Lesson 04 are completed

--- src/by/it/busel/lesson04/TaskB2.java
+++ src/by/it/busel/lesson04/TaskB2.java
@@ -1,5 +1,7 @@
 package by.it.busel.lesson04;
 
+import java.util.Scanner;
+
 /*
 Таблица умножения
 Вывести на экран таблицу умножения size х size используя цикл while.
@@ -24,5 +26,26 @@ size прочитать c клавиатуры. Числа разделить п
 3. Программа должна выводить таблицу умножения size х size.
  */
 public class TaskB2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int y, x;
+        /*for (y =1; y <=size; y++){
+            for (x=1; x<=size; x++){
+                System.out.print((y*x) + " ");
+            }
+            System.out.println();
+        }*/
+        y=1;
+        while(y<=size){
+            x=1;
+            while(x<=size){
+                System.out.print((y*x) +" ");
+                x++;
+            };
+            y++;
+            System.out.println();
+        }
+    }
 
 }
