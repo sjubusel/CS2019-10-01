3007eddbe8c9f51fc82f09169d931e71f4b160a3
sjubusel <sju.busel@gmail.com>
2019-10-12 21:24:43 +0200

    CS - Tasks A, Tasks B, Task C1 of Lesson 04 are completed

--- src/by/it/busel/lesson04/TaskC1.java
+++ src/by/it/busel/lesson04/TaskC1.java
@@ -1,5 +1,9 @@
 package by.it.busel.lesson04;
 
+import com.sun.javaws.IconUtil;
+
+import java.util.Scanner;
+
 /*
 Напишите программу которая спрашивает у пользователя:
 Какую вы хотите зарплату в $$$?
@@ -37,6 +41,83 @@ package by.it.busel.lesson04;
 
 */
 public class TaskC1 {
+    public static void main(String[] args) {
+        System.out.println("Какую вы хотите зарплату в $$$?");
+        Scanner scanner = new Scanner(System.in);
+        int salary = scanner.nextInt();
 
 
+        if (salary < 300 || salary > 3000) {
+            System.out.println("Мы вам перезвоним!");
+        } else {
+            done:
+            for (int i = 0; i <= 14; i++) {
+                double dsalary = salary;
+                switch (i) {
+                    case 0:
+                        System.out.println("За месяц 0 начислено $0.0");
+                        break;
+                    case 1:
+                        System.out.println("За январь начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 2:
+                        System.out.println("За февраль начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 3:
+                        System.out.println("За март начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 4:
+                        System.out.println("За апрель начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 5:
+                        System.out.println("За май начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 6:
+                        System.out.println("За июнь начислено $" + dsalary);
+                        if (dsalary == 666) break done;
+                        break;
+                    case 7:
+                        System.out.println("За июль начислено $" + dsalary);
+                        if (dsalary == 666) break done;
+                        break;
+                    case 8:
+                        System.out.println("За август начислено $" + dsalary);
+                        if (dsalary == 666) break done;
+                        break;
+                    case 9:
+                        System.out.println("За сентябрь начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 10:
+                        System.out.println("За октябрь начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 11:
+                        System.out.println("За ноябрь начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 12:
+                        System.out.println("За декабрь начислено $" + (dsalary + dsalary * 0.5));
+                        if ((dsalary + dsalary * 0.5) == 666) break done;
+                        break;
+                    case 13:
+                        System.out.println("За месяц 13 начислено $0.0");
+                        break;
+                    case 14:
+                        System.out.println("За месяц 14 начислено $0.0");
+                        break;
+                }
+            }
+        }
+
+
+    }
 }
+
+
+
