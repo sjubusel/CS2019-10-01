9dc9235974ccf18e9a808d8d84c635dc535f3449
sjubusel <sju.busel@gmail.com>
2019-10-12 20:40:17 +0200

    CS - Tasks A, Tasks B of Lesson 04 are completed

--- src/by/it/busel/lesson04/TaskB3.java
+++ src/by/it/busel/lesson04/TaskB3.java
@@ -25,7 +25,16 @@ package by.it.busel.lesson04;
  */
 
 
+import java.util.Scanner;
 
 public class TaskB3 {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i1 = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; (sum + i) <=i1; i++){// 1 2 3 4 5
+            sum = sum + i; // 1 1+2=3 3 +3= 6 6+ 4=10 10 +5
+            System.out.print(sum + ","); //1, 3, 6,  10, 15
+    }
+    }
 }
