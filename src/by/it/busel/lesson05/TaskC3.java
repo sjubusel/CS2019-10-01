2d14c167989c9bd75f4784b881bbc01baba9922d
sjubusel <sju.busel@gmail.com>
2019-10-17 01:43:10 +0200

    L05(A, B, C1, C2)

--- src/by/it/busel/lesson05/TaskC3.java
+++ src/by/it/busel/lesson05/TaskC3.java
@@ -5,15 +5,43 @@ package by.it.busel.lesson05;
 и выводит их в убывающем порядке. Скорость O(n log n)
 */
 
-public class TaskC3 {
-    //Напишите тут ваш код
 
+import java.util.Scanner;
+
+public class TaskC3 {
+
+    // задача не решена требуемым алгоритмом
+
+    /*public static void main(String[] args) {
+        int[] integers = new int[20];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = scanner.nextInt();
+//            System.out.print(integers[i] + " ");
+        }
+        sort(integers);
+        for (int i = 0; i < integers.length; i++) {
+            int j = integers.length - 1 - i;
+            System.out.println(integers[j]);
+
+        }
+
+    }
 
 
 
 
     public static void sort(int[] array) {
-        //Напишите тут ваш код
-    }
+        int tempint;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = array.length - 1; j >= i; j--) {
+                if (array[j - 1] > array[j]) {
+                    tempint = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tempint;
+                }
+            }
+        }
+    }*/
 
 }
