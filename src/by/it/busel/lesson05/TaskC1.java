f431114bec8df106e05c904a4b62d7977702efe4
sjubusel <sju.busel@gmail.com>
2019-10-16 20:22:07 +0200

    L05(A, B, C1), L04(A B C), L03 (A B C), L02 (A B C) are correctly completed

--- src/by/it/busel/lesson05/TaskC1.java
+++ src/by/it/busel/lesson05/TaskC1.java
@@ -9,13 +9,54 @@ package by.it.busel.lesson05;
 3.  Используя метод printList выведите ваши три списка на экран.
     Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
-
+//1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TaskC1 {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arlist = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            int integer = scanner.nextInt();
+            arlist.add(integer);
+        }
+        ArrayList<Integer> arlistK3 = new ArrayList<>();
+        ArrayList<Integer> arlistK2 = new ArrayList<>();
+        ArrayList<Integer> arlistKOthers = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            if (arlist.get(i) % 3 == 0) {
+                arlistK3.add(arlist.get(i));
+            }
+            if (arlist.get(i) % 2 == 0){
+                arlistK2.add(arlist.get(i));
+            }
+            if (arlist.get(i) % 3 !=0 && arlist.get(i) % 2 !=0){
+                arlistKOthers.add(arlist.get(i));
+            }
+        }
+        printList(arlistK3);
+        printList(arlistK2);
+        printList(arlistKOthers);
+        /*for (int i = 0; i < arlistK3.size(); i++) {
+            System.out.print(arlistK3.get(i) + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arlistK2.size(); i++) {
+            System.out.print(arlistK2.get(i) + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arlistKOthers.size(); i++) {
+            System.out.print(arlistKOthers.get(i) + " ");
+        }
+        System.out.println();*/
 
+
+
+        /*for (int i = 0; i < 20; i++) {
+            System.out.println(arlist.get(i));
+        }*/
     }
 
     private static void printList(List<Integer> list) {
